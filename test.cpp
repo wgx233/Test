1d25ea33c730f97d435dffa78c2898abbbc56041
Guangxing Wu <39212533+wgx233@users.noreply.github.com>
2020-10-26 15:09:39 +0800

    Two Dimentional Difference
    A data structure to modify values of a submatrix area of a matrix in O(1).

---
+++ test.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define mp make_pair
+#define ALL(x) x.begin(), x.end()
+
+typedef long long ll;
+// const double PI = 4*atan(1.);
+// const int INF = 0x3f3f3f3f;
+const int maxn = 5e3 + 10;
+
+int a[maxn][maxn];
+int d[maxn][maxn];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, m, q;
+    cin >> n >> m >> q;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            cin >> a[i][j];
+            d[i][j] = a[i][j] - a[i - 1][j] - a[i][j - 1] + a[i - 1][j - 1];
+        }
+    }
+    while (q--) {
+        int x, y, xx, yy, c;
+        cin >> x >> y >> xx >> yy >> c;  // (x, y) ~ (xx, yy) add c
+        d[x][y] += c;
+        d[x][yy + 1] -= c;
+        d[xx + 1][y] -= c;
+        d[xx + 1][yy + 1] += c;
+    }
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j) {
+            d[i][j] += d[i - 1][j] + d[i][j - 1] - d[i - 1][j - 1];
+            cout << d[i][j] << " \n"[j == m];
+        }
+    return 0;
+}
